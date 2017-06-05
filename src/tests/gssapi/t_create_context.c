ea1f64dde5e9b6c16bb3eaf48d270bce30870b5f
Alexander Scheel <alexander.m.scheel@gmail.com>
2017-06-05 12:57:18 -0500

    Fixed test cases, initial code

--- src/tests/gssapi/t_create_context.c
+++ src/tests/gssapi/t_create_context.c
@@ -21,18 +21,44 @@ t_gss_create_context(void)
     OM_uint32 maj_stat;
     OM_uint32 min_stat;
     gss_ctx_id_t context = GSS_C_NO_CONTEXT;
+    stub_gss_ctx_id_rec *check;
     gss_ctx_id_t *context_handle = NULL;
 
     maj_stat = gss_create_sec_context(&min_stat, NULL);
-    check_gsserr("t_gss_create_context()", maj_stat, min_stat);
+    assert(maj_stat == GSS_S_FAILURE);
 
     maj_stat = gss_create_sec_context(&min_stat, &context);
     check_gsserr("t_gss_create_context()", maj_stat, min_stat);
+    assert(context != GSS_C_NO_CONTEXT);
+    check = (stub_gss_ctx_id_rec *)context;
+    assert(check != NULL);
+    assert(check->magic_num == STUB_MAGIC_ID);
+    assert(check->req_flags == 0);
+    assert(check->ret_flags == 0);
 
-    free(context);
+    free(check);
+    check = NULL;
+
+    maj_stat = gss_create_sec_context(&min_stat, context_handle);
+    assert(maj_stat = GSS_S_FAILURE);
+
+    context_handle = malloc(sizeof(gss_ctx_id_t));
+    if (context_handle == NULL) {
+        return 1;
+    }
 
     maj_stat = gss_create_sec_context(&min_stat, context_handle);
     check_gsserr("t_gss_create_context()", maj_stat, min_stat);
+
+    assert(*context_handle != NULL);
+    check = (stub_gss_ctx_id_rec *)(*context_handle);
+    assert(check != NULL);
+    assert(check->magic_num == STUB_MAGIC_ID);
+    assert(check->req_flags == 0);
+    assert(check->ret_flags == 0);
+
+    free(check);
+    free(context_handle);
 
     return 0;
 }
