2eb37faa5c69f8445969473baa5e550dd3e3b35a
Alexander Scheel <alexander.m.scheel@gmail.com>
2017-06-05 13:25:30 -0500

    Added set_context_flags to build, test cases

--- src/lib/gssapi/mechglue/g_set_context_flags.c
+++ src/lib/gssapi/mechglue/g_set_context_flags.c
@@ -30,17 +30,21 @@ gss_set_context_flags(minor_status,
                       ret_flags)
 
 OM_uint32    *minor_status;
-gss_ctx_id_t *context;
+gss_ctx_id_t  context;
 uint64_t      req_flags;
 uint64_t      ret_flags;
 {
     stub_gss_ctx_id_rec *ctx;
 
-    if (ctx_handle == NULL) {
+    if (context == NULL) {
         return GSS_S_FAILURE;
     }
 
     ctx = (stub_gss_ctx_id_rec *)context;
+
+    if (ctx == NULL) {
+        return GSS_S_FAILURE;
+    }
 
     if (ctx->magic_num != STUB_MAGIC_ID) {
         return GSS_S_FAILURE;
