02c3b92295bf89b22a4c7b8d80641fc2a24f0640
Alexander Scheel <alexander.m.scheel@gmail.com>
2017-06-05 11:56:29 -0400

    Minor changes to create_sec_context, added set_context_flags

--- src/lib/gssapi/mechglue/g_set_context_flags.c
+++ src/lib/gssapi/mechglue/g_set_context_flags.c
@@ -1,7 +1,7 @@
 /* #pragma ident	                                                         */
 
 /*
- *  glue routine for gss_create_sec_context
+ *  glue routine for gss_set_context_flags
  */
 
 #include "mglueP.h"
@@ -19,29 +19,35 @@
  * Defined:
  * https://tools.ietf.org/html/draft-ietf-kitten-channel-bound-flag-01
  *
- * Section 2.1
+ * Section 2.2
  * 
  * See src/lib/gssapi/generic/gssapi_ext.h for type definitions.
  */
 OM_uint32 KRB5_CALLCONV
-gss_create_sec_context(minor_status,
-                       context)
+gss_set_context_flags(minor_status,
+                      context,
+                      req_flags,
+                      ret_flags)
 
 OM_uint32    *minor_status;
 gss_ctx_id_t *context;
+uint64_t      req_flags;
+uint64_t      ret_flags;
 {
     stub_gss_ctx_id_rec *ctx;
 
-    *minor_status = 0;
-
-    ctx = calloc(sizeof(stub_gss_ctx_id_rec), 0);
-    if (ctx == NULL) {
-        return GSS_S_UNAVAILABLE;
+    if (ctx_handle == NULL) {
+        return GSS_S_FAILURE;
     }
 
-    ctx->magic_num = STUB_MAGIC_ID;
+    ctx = (stub_gss_ctx_id_rec *)context;
 
-    context = (gss_ctx_id_t *)ctx;
+    if (ctx->magic_num != STUB_MAGIC_ID) {
+        return GSS_S_FAILURE;
+    }
+
+    ctx->req_flags = req_flags;
+    ctx->ret_flags = ret_flags;
 
     return GSS_S_COMPLETE;
 }
