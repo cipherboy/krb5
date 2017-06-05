cd95fb51c5e733093532a097d2e6b57b3764b993
Alexander Scheel <alexander.m.scheel@gmail.com>
2017-06-05 08:20:18 -0500

    Patch in gss_create_sec_context

--- src/lib/gssapi/mechglue/mglueP.h
+++ src/lib/gssapi/mechglue/mglueP.h
@@ -702,6 +702,11 @@ typedef struct gss_config {
 	    int				/* iov_count */
 	);
 
+        OM_uint32       (KRB5_CALLCONV *gss_create_sec_context)
+        (
+            OM_uint32 *,                /* minor status */
+            gss_ctx_id_t *              /* context */
+        );
 } *gss_mechanism;
 
 /*
