5e31c59c6f6484e56141d476f9bbdb4a65f2bc52
Alexander Scheel <alexander.m.scheel@gmail.com>
2017-06-07 14:01:25 -0400

    New test towards implementing krb5_gss_create_sec_context

---
+++ src/lib/gssapi/krb5/set_context_flags.c
@@ -0,0 +1,32 @@
+/* -*- mode: c; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
+#include "k5-int.h"
+#include "gssapiP_krb5.h"
+
+#include <assert.h>
+
+
+OM_uint32 KRB5_CALLCONV
+krb5_gss_set_context_flags(OM_uint32 *minor_status, gss_ctx_id_t context, uint64_t req_flags, uint64_t ret_flags)
+{
+    /*krb5_error_code kerr;
+
+    krb5_context new_context;
+    if (*context == GSS_C_NO_CONTEXT) {
+        kerr = krb5_gss_init_context(&new_context);
+        if (kerr) {
+            *minor_status = kerr;
+            return GSS_S_FAILURE;
+        }
+        if (GSS_ERROR(kg_sync_ccache_name(new_context, minor_status))) {
+            save_error_info(*minor_status, new_context);
+            krb5_free_context(new_context);
+            return GSS_S_FAILURE;
+        }
+    } else {
+        context = ((krb5_gss_ctx_id_rec *)*context_handle)->k5_context;
+    }
+
+    krb5_gss_init_context()*/
+    return 0;
+}
--- src/tests/gssapi/t_create_exchange.c
+++ src/tests/gssapi/t_create_exchange.c
@@ -11,7 +11,12 @@
 #include <string.h>
 #include <assert.h>
 
+#include "k5-int.h"
+#include "k5-platform.h"
 #include "common.h"
+#include "mglueP.h"
+#include "gssapiP_krb5.h"
+#include "gssapi_ext.h"
 
 static int
 t_gss_handshake_create_init(gss_name_t target_name)
@@ -151,9 +156,86 @@ t_gss_handshake_create_both(gss_name_t target_name)
 }
 
 
+static int
+t_gss_krb5_struct_empty_fireworks(gss_name_t target_name)
+{
+    OM_uint32 maj_stat = 0;
+    OM_uint32 min_stat = 0;
+    gss_ctx_id_t context = GSS_C_NO_CONTEXT;
+    gss_union_ctx_id_t union_ctx = NULL;
+    krb5_context krb5_inner_context;
+    krb5_error_code kerr;
+    krb5_gss_ctx_id_rec *ctx;
+    gss_OID mech = &mech_krb5;
+    gss_buffer_desc init_token;
+    gss_cred_id_t cred = GSS_C_NO_CREDENTIAL;
+    gss_buffer_desc accept_token;
+    gss_ctx_id_t init_context = GSS_C_NO_CONTEXT;
+    gss_ctx_id_t accept_context = GSS_C_NO_CONTEXT;
+
+        maj_stat = gss_acquire_cred(&min_stat, GSS_C_NO_NAME, 0, GSS_C_NO_OID_SET,
+                                    GSS_C_BOTH, &cred, NULL, NULL);
+        check_gsserr("t_gss_handshake_create_both(0)", maj_stat, min_stat);
+
+
+    maj_stat = gss_create_sec_context(&min_stat, &context);
+    check_gsserr("t_gss_create_context()", maj_stat, min_stat);
+    assert(context != GSS_C_NO_CONTEXT);
+
+    union_ctx = (gss_union_ctx_id_t)context;
+    kerr = krb5_gss_init_context(&krb5_inner_context);
+    if (kerr) {
+        return 1;
+    }
+
+    ctx = (krb5_gss_ctx_id_rec *)calloc(sizeof(krb5_gss_ctx_id_rec), 1);
+    if (ctx == NULL) {
+        return 2;
+    }
+
+    ctx->magic = KG_CONTEXT;
+    if (krb5_auth_con_init(krb5_inner_context, &ctx->auth_context))
+        return 3;
+    krb5_auth_con_setflags(krb5_inner_context, ctx->auth_context,
+                           KRB5_AUTH_CONTEXT_DO_SEQUENCE);
+
+
+    ctx->initiate = 1;
+    union_ctx->internal_ctx_id = (gss_ctx_id_t) ctx;
+    ctx->k5_context = krb5_inner_context;
+
+    printf("%p %p\n", krb5_inner_context, ctx->k5_context);
+
+    /* Get the initial context token. */
+    maj_stat = gss_init_sec_context(&min_stat, GSS_C_NO_CREDENTIAL,
+                                    &init_context, target_name, mech, 0, 0,
+                                    GSS_C_NO_CHANNEL_BINDINGS, GSS_C_NO_BUFFER,
+                                    NULL, &init_token, NULL, NULL);
+
+    check_gsserr("t_gss_handshake_create_both(3)", maj_stat, min_stat);
+    assert(maj_stat == GSS_S_COMPLETE);
+    assert(init_token.length != 0);
+
+    /* Process this token into an acceptor context, then discard it. */
+    maj_stat = gss_accept_sec_context(&min_stat, &accept_context,
+                                      cred, &init_token,
+                                      GSS_C_NO_CHANNEL_BINDINGS, NULL,
+                                      NULL, &accept_token, NULL, NULL, NULL);
+    check_gsserr("t_gss_handshake_create_both(4)", maj_stat, min_stat);
+    assert(maj_stat == GSS_S_COMPLETE);
+
+    (void)gss_release_buffer(&min_stat, &init_token);
+    (void)gss_release_buffer(&min_stat, &accept_token);
+
+    (void)gss_delete_sec_context(&min_stat, &accept_context, NULL);
+    (void)gss_delete_sec_context(&min_stat, &init_context, NULL);
+    return 0;
+}
+
 int
 main(int argc, char *argv[])
 {
+    int ret_val = 0;
     gss_name_t target_name;
     OM_uint32 min_stat;
 
@@ -171,6 +253,11 @@ main(int argc, char *argv[])
 
     assert(t_gss_handshake_create_both(target_name) == 0);
     printf("t_gss_handshake_create_both... ok\n");
+
+    ret_val = t_gss_krb5_struct_empty_fireworks(target_name);
+    printf("%d\n", ret_val);
+    assert(ret_val == 0);
+    printf("t_gss_krb5_struct_empty_fireworks()... ok\n");
 
     (void)gss_release_name(&min_stat, &target_name);
 }
